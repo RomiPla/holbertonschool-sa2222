ac27cd788c2e1d91eafd678284e50c46ee8d6ce8
root <plaromina1997@gmail.com>
2023-09-07 18:01:01 -0700

    0-bubble_sort.c

---
+++ sort.h
@@ -0,0 +1,9 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include <stddef.h>
+void bubble_sort(int *array, size_t size);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+
+#endif
