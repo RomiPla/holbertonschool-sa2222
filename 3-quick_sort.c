ebf9636991611daa133e2b12f9b4573df50a4497
iamara <iamarabarrios457@gmail.com>
2023-09-08 18:05:34 -0700

    exercise 3-quick_sort.c

---
+++ 3-quick_sort.c
@@ -0,0 +1,76 @@
+#include "sort.h"
+
+/**
+ * swap_ints - swaps the value of two integers via pointers
+ * @a: int
+ * @b: int
+ */
+
+void swap_ints(int *a, int *b)
+{
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * lomuto_partition - split the arrangement
+ * @array: int
+ * @size: size_t
+ * @left: int
+ * @right: int
+ * Return: above
+ */
+
+int lomuto_partition(int *array, size_t size, int left, int right)
+{
+	int *pivot = array + right, above = left;
+
+	for (int below = left; below < right; below++)
+	{
+		if (above != below)
+		{
+			swap_ints(array + below, array + above);
+			print_array(array, size);
+			above++;
+		}
+	}
+
+		if (array[above] > *pivot)
+		{
+			swap_ints(array + above, pivot);
+			print_array(array, size);
+		}
+	return (above);
+}
+
+/**
+ * lomuto_sort - minor on the left and major on the right
+ * @array: int
+ * @size: size_t
+ * @left: int
+ * @right: int
+ */
+void lomuto_sort(int *array, size_t size, int left, int right)
+{
+	if (left < right)
+	{
+		int part = lomuto_partition(array, size, left, right);
+
+		lomuto_sort(array, size, left, part - 1);
+		lomuto_sort(array, size, part + 1, right);
+	}
+}
+
+/**
+ * quick_sort - sorts n array of integers in ascending order
+ * @array: int
+ * @size: size-t
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	if (array && size > 1)
+		lomuto_sort(array, size, 0, size - 1);
+}
+
--- sort.h
+++ sort.h
@@ -24,5 +24,9 @@ void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void swap_ints(int *a, int *b);
+int lomuto_partition(int *array, size_t size, int left, int right);
+void lomuto_sort(int *array, size_t size, int left, int right);
+void quick_sort(int *array, size_t size);
 
 #endif
